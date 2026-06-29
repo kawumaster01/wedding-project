0534f725e8215c9c9a08d0a179d667e1c51ce543
kawumaster <kawumaster55@gmail.com>
2026-06-28 18:48:22 -0700

    include js style

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Safiyanu 💖 Asmau Wedding</title>
 
-    @vite('resources/css/app.css')
-
+    <!-- @vite('resources/css/app.css') -->
+<script src="https://cdn.tailwindcss.com"></script>
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap" rel="stylesheet">
 </head>
